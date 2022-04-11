89a1df558c2bf1558e6f94a53062b7676fe6be03
wjdals3406 <sssi6792@gmail.com>
2022-04-11 09:36:07 +0900

    [ADD]

---
+++ 이코테/그리디/볼링공고르기.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import sys
+from itertools import combinations
+
+#3 2 1 1 9 -> 8
+n, m = map(int,sys.stdin.readline().split())
+ball = list(map(int,sys.stdin.readline().split()))
+
+perm = list(combinations(ball, 2))
+for i in perm:
+    if i[0] == i[1]:
+        perm.remove(i)
+        
+print(len(perm))
+        
+
