062c95d3950d11dfc7f576be9518704eaa1ed166
jeongmin <sssi6792@gmail.com>
2023-01-10 18:01:38 +0900

    Level_3

--- 프로그래머스스쿨/베스트앨범.py
+++ 프로그래머스스쿨/베스트앨범.py
@@ -34,3 +34,34 @@ def solution(genres, plays):
 genres = ["classic", "pop", "classic", "classic", "pop"]
 plays = [500, 600, 150, 800, 2500]
 print(solution(genres, plays))
+
+
+#다른 사람의 풀이
+# def solution(genres, plays):
+#     answer = []
+    
+#     d = defaultdict(list)
+#     for g,p,i in zip(genres, plays, range(len(plays))):
+#         d[g].append([p,i])
+            
+#     genreSort =sorted(list(d.keys()), key= lambda x: sum( map(lambda y: y[0],d[x])), reverse = True)
+#     for g in genreSort:
+#         temp = [e[1] for e in sorted(d[g],key= lambda x: (-x[0], x[1]))]
+#         answer += temp[:min(len(temp),2)]
+#     return answer
+
+# def solution(genres, plays):
+#     answer = []
+
+#     dic1 = defaultdict(list)
+#     dic2 = defaultdict(list)
+    
+#     for g,p,i in zip(genres, plays, range(len(plays))):
+#         dic1[g].append([i,p])
+#         dic2[g].append(p)
+
+#     for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
+#         for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
+#             answer.append(i)
+
+#     return answer
