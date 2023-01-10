d27255dafb44296e2a3ba25280a06d346bdb4e88
jeongmin <sssi6792@gmail.com>
2023-01-10 17:33:33 +0900

    Level_3

--- 프로그래머스스쿨/베스트앨범.py
+++ 프로그래머스스쿨/베스트앨범.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 def solution(genres, plays):
     answer = []
-    
+
     dic = defaultdict(list)
     for g,p in zip(genres, plays):
         dic[g].append(p)
-        
+
     data = sorted(dic.items(), key = lambda x : -sum(x[1]))
     for i in range(len(data)):
         play = sorted(data[i][1])
@@ -13,10 +13,22 @@ def solution(genres, plays):
         while play:
             if cnt == 2:
                 break
-            
-            answer.append(plays.index(play.pop()))
+
+            num = play.pop()
+
+            if plays.count(num) > 1:
+                for j in range(len(plays)):
+                    if plays[j] == num and genres[j] == data[i][0]:
+                        if j in answer:
+                            continue
+                        answer.append(j)
+
+                        break
+            else:
+                answer.append(plays.index(num))
+
             cnt += 1
-        
+
     return answer
 
 genres = ["classic", "pop", "classic", "classic", "pop"]
