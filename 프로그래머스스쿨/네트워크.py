82f41602da25edb51676cc986f9280a111ab7bdc
jeongmin <sssi6792@gmail.com>
2023-01-14 16:26:11 +0900

    Level_3

---
+++ 프로그래머스스쿨/네트워크.py
@@ -0,0 +1,31 @@
+from collections import deque
+def solution(n, computers):
+    answer = 0
+    visited = [0 for _ in range(n)] # 네트워크 별로, 1이상의 수를 가지기
+    graph = [[] for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            if i==j:
+                continue
+            if computers[i][j]:
+                graph[i].append(j)
+    
+    def bfs(start,num):
+        que = deque([start])
+        visited[start] = num
+        while que:
+            node = que.popleft()
+            for i in graph[node]:
+                if not visited[i]:
+                    visited[i] = num
+                    que.append(i)
+                
+    num = 1
+    for i in range(n):
+        if not visited[i]:
+            bfs(i,num)
+        num += 1
+    
+    
+    return len(set(visited))
