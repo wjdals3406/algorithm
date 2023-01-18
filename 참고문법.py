0c32726788b5925401ab28038a6232a0586b82a5
jeongmin <sssi6792@gmail.com>
2023-01-18 20:39:52 +0900

    참고문법

--- 참고문법.py
+++ 참고문법.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 reports = {x : 0 for x in [0,1,2]}
 print(reports)
 
@@ -12,3 +13,9 @@ def func():
     result += 1
     
 func()
+
+
+#리스트 인덱싱할 때 리스트 길이보다 큰 인덱스를 접근할라 하면 IndexError 나지만,
+#list[10000:] 이렇게 큰값: 하면 에러안남 
+a = [1,2,3,4,5]
+print(a[10:]) #결과 : []
