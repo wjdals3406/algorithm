28f83b2742fa49611ff22ba08c04bd80b2aed220
jeongmin <sssi6792@gmail.com>
2023-02-05 21:38:31 +0900

    Gold3 배열 회전 삼성기출

--- baekjoon/구현/20058_마법사상어와파이어스톰.py
+++ baekjoon/구현/20058_마법사상어와파이어스톰.py
@@ -5,17 +5,23 @@ from collections import deque
 n,q = map(int, sys.stdin.readline().split())
 data = [list(map(int, sys.stdin.readline().split())) for _ in range(2**n)]
 qlist = list(map(int, sys.stdin.readline().split()))
+size = 2**n
 #1.배열 90도 회전
 #2.남아있는 얼음 A[r][c]의 합 : sum(map(sum,data))
 #3.남아있는 얼음 중 가장 큰 덩어리가 차지하는 칸의 개수 구하기
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-visited = [[0] * (2**n) for _ in range(2**n)]
 
-def turn_90(l): #Li가 0일 때는 아무것도 안하나..?
+def rotate_90(row,column,size): 
+    temp = []
+    #data[row:row+size][column:column+size]이렇게 접근 불가능 / 따라서 다음과 같이 append 해야함
+    for i in range(row,row+size):
+        temp.append(data[i][column:column+size])
     
-    
-
+    for r in range(size):
+        for c in range(size):
+            data[row+c][column + size-r-1] = temp[r][c]
+                    
 def bfs(x,y): 
     que = deque([[x,y]])
     cnt = 1
@@ -25,24 +31,45 @@ def bfs(x,y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >= 2**n or ny < 0 or ny >= 2**n:
+            if nx < 0 or nx >= size or ny < 0 or ny >= size:
                 continue
             if not visited[nx][ny] and data[nx][ny] > 0:
                 visited[nx][ny] = cnt + 1
                 cnt += 1
                 que.append([nx,ny])
 
+for l in qlist: #배열 회전 수
+    if l > 0:
+        for r in range(0, size, 2**l):
+            for c in range(0, size, 2**l):
+                rotate_90(r,c,2**l) #회전할 배열(시작행, 끝행, 부분격자 size)
+    
+    rmlist = set()
+    for i in range(size):
+        for j in range(size):
+            if data[i][j] > 0:
+                cnt = 0
+                for k in range(4):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    if nx < 0 or nx >= size or ny < 0 or ny >= size:
+                        continue
+                    if data[nx][ny] > 0:
+                        cnt += 1
+                if cnt <= 2:
+                    rmlist.add((i,j))
+        
+    for x,y in rmlist:
+        data[x][y] -= 1
+                    
 
-for i in range(2**n):
-    for j in range(2**n):
-        if not visited[i][j]:
+visited = [[0] * size for _ in range(size)]
+for i in range(size):
+    for j in range(size):
+        if not visited[i][j] and data[i][j] > 0:
             bfs(i,j)
+            
 print(sum(map(sum,data)))
 print(max(map(max,visited)))
 
     
-            
-
-
-
-
