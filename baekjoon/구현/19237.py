8db8fb2d54e4ffcfd0461e306956e241e2f50a22
jeongmin <sssi6792@gmail.com>
2023-02-06 15:02:00 +0900

    Gold3 배열 회전 삼성기출

--- baekjoon/구현/19237.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-n,m = map(int, sys.stdin.readline().split())
-data = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-#상어의 위치 :  ((N+1)/2, (N+1)/2)
