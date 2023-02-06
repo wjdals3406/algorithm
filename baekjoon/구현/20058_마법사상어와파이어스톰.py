82b66d4ca972cfa54357f05cbe578056adcad5a6
jeongmin <sssi6792@gmail.com>
2023-02-06 16:24:16 +0900

    Gold3 배열 회전 삼성기출

--- baekjoon/구현/20058_마법사상어와파이어스톰.py
+++ baekjoon/구현/20058_마법사상어와파이어스톰.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
-#???? ?? A[r][c]? ? / ???? ?? ? ?? ? ???? ???? ?? ?? ???
+#남아있는 얼음 A[r][c]의 합 / 남아있는 얼음 중 가장 큰 덩어리가 차지하는 칸의 개수 구하기
 import sys
 from collections import deque
 n,q = map(int, sys.stdin.readline().split())
 data = [list(map(int, sys.stdin.readline().split())) for _ in range(2**n)]
 qlist = list(map(int, sys.stdin.readline().split()))
 size = 2**n
-#1.?? 90? ??
-#2.???? ?? A[r][c]? ? : sum(map(sum,data))
-#3.???? ?? ? ?? ? ???? ???? ?? ?? ???
+#1.배열 90도 회전
+#2.남아있는 얼음 A[r][c]의 합 : sum(map(sum,data))
+#3.남아있는 얼음 중 가장 큰 덩어리가 차지하는 칸의 개수 구하기
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 def rotate_90(row,column,size): 
     temp = []
-    #data[row:row+size][column:column+size]??? ?? ??? / ??? ??? ?? append ???
+    #data[row:row+size][column:column+size]이렇게 접근 불가능 / 따라서 다음과 같이 append 해야함
     for i in range(row,row+size):
         temp.append(data[i][column:column+size])
     
@@ -38,11 +38,11 @@ def bfs(x,y):
                 cnt += 1
                 que.append([nx,ny])
 
-for l in qlist: #?? ?? ?
+for l in qlist: #배열 회전 수
     if l > 0:
         for r in range(0, size, 2**l):
             for c in range(0, size, 2**l):
-                rotate_90(r,c,2**l) #??? ??(???, ??, ???? size)
+                rotate_90(r,c,2**l) #회전할 배열(시작행, 끝행, 부분격자 size)
     
     rmlist = set()
     for i in range(size):
@@ -71,3 +71,5 @@ for i in range(size):
             
 print(sum(map(sum,data)))
 print(max(map(max,visited)))
+
+    
