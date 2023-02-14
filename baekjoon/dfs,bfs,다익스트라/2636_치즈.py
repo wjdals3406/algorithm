93ede0b9ad099b2907a349cc0f078846ea4d20de
jeongmin <sssi6792@gmail.com>
2023-02-14 23:39:42 +0900

    Gold4 치즈

--- baekjoon/dfs,bfs,다익스트라/2636_치즈.py
+++ baekjoon/dfs,bfs,다익스트라/2636_치즈.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import queue
 import sys
 from collections import deque
 n,m = map(int, sys.stdin.readline().split())
@@ -7,39 +6,43 @@ data = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def dfs():
-    que = deque([(0,0)])
+time = 0
+
+def count_cheese():
+    return sum(map(sum, data))
+
+def bfs():
+    que = deque([[0,0]])
     visited = [[0] * m for _ in range(n)]
+    visited[0][0] = 1
     melt = []
+    
     while que:
         x,y = que.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if data[nx][ny] == 1 and not visited[nx][ny]:
+            
+            if not visited[nx][ny] and data[nx][ny] == 0:
+                que.append([nx,ny])
+                visited[nx][ny] = 1
+            
+            elif not visited[nx][ny] and data[nx][ny] == 1:
                 melt.append((nx,ny))
-            elif data[nx][ny] == 0 and not visited[nx][ny]:
-                que.append((nx,ny))
-            visited[nx][ny] = 1
-    return melt         
-
-melt = [0]
-time = 0
-while True:
-    melt = dfs()
+                visited[nx][ny] = 1
+                
     for x,y in melt:
         data[x][y] = 0
-    res = 0
-    for i in data:
-        res += sum(i)
+        
+while count_cheese():
+    cnt = count_cheese()
+    bfs()
     time += 1
-    if res == 0:
-        break
     
-    
+
 print(time)
-print(len(melt))
+print(cnt)
     
     
